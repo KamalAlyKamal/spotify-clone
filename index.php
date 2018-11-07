ddb415ea88176bb42338452c084140c81c0d0aea
Kamal Aly <kamalaly59@yahoo.com>
2018-11-07 02:25:15 +0200

    Initial container styling

--- index.php
+++ index.php
@@ -18,10 +18,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Spotify: Music for everyone</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <link rel="stylesheet" type="text/css" media="screen" href="main.css" /> -->
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/style.css" />
     <!-- <script src="main.js"></script> -->
 </head>
 <body>
-    Hello
+    <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+            
+        </div>
+    </div>
 </body>
 </html>
