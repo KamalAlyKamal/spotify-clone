e310de46fc8478e35a3eda7b0b54add093ab4f3f
Kamal Aly <kamalaly59@yahoo.com>
2018-11-23 18:11:22 +0200

    Output albums images and titles

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php include("Includes/header.php"); ?>
 
+<h1 class="pageHeadingBig">You Might Also Like</h1>
+
+<div class="gridViewContainer">
+    <?php 
+        $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+        while ($row = mysqli_fetch_array($albumQuery)) {
+            echo    "<div class='gridViewItem'>
+                        <img src='" . $row['artworkPath'] . "'>
+                        <div class='gridViewInfo'>"
+                            . $row['title'] .
+                        "</div>
+                    </div>";
+        }
+    ?>
+</div>
+
 <?php include("Includes/footer.php"); ?>
