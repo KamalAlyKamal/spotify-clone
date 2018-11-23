228338b2b20d0b21f0c5eed95959ebec8c963ac7
Kamal Aly <kamalaly59@yahoo.com>
2018-11-23 19:05:06 +0200

    Style album header section

--- album.php
+++ album.php
@@ -14,4 +14,15 @@
 ?>
 
 
+<div class="entityInfo">
+    <div class="leftSection">
+        <img src="<?php echo $album->getArtworkPath(); ?>" alt="Album">
+    </div>
+
+    <div class="rightSection">
+        <h2><?php echo $album->getTitle(); ?></h2>
+        <span>By <?php echo $artist->getName(); ?></span>
+    </div>
+</div>
+
 <?php include("Includes/footer.php"); ?>
