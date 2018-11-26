7d80a6e926b1fcf45d971d013ce20e7059862657
Kamal Aly <kamalaly59@yahoo.com>
2018-11-26 05:11:28 +0200

    Get and format song duration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,19 @@
 var currentPlaylist = [];
 var audioElement;
 
+function formatTime(seconds) {
+    var time = Math.round(seconds);
+    var minutes = Math.floor(time / 60);
+    var seconds = time - (minutes * 60);
+
+    // to print 0 before 3 to be like 6:03
+    var extraZero;
+
+    extraZero = (seconds < 10) ? "0" : "";
+
+    return minutes + ":" + extraZero + seconds;
+}
+
 function Audio() {
     this.currentlyPlaying;
     this.audio = document.createElement('audio');
@@ -17,4 +30,10 @@ function Audio() {
     this.pause = function() {
         this.audio.pause();
     }
+
+    // Event listener for remaining time
+    this.audio.addEventListener('canplay', function() {
+        var duration = formatTime(this.duration);
+        $('.progressTime.remaining').text(duration);
+    });
 }
