d1ae7c3466ea86177d5800431cbcbd3f94e0563c
Kamal Aly <kamalaly59@yahoo.com>
2018-11-28 01:23:20 +0200

    Search when user stops typing

--- search.php
+++ search.php
@@ -14,5 +14,22 @@
 
 <div class="searchContainer">
     <h4>Search for a song, artist, album, or a playlist</h4>
-    <input type="text" class="searchInput" value="<?php echo $query; ?>" placeholder="Search...">
+    <input type="text" class="searchInput" value="<?php echo $query; ?>" placeholder="Search..." onfocus="this.value = this.value">
 </div>
+
+<script>
+    // Refocus input on page reload
+    $(".searchInput").focus();
+
+    // IIFE to reload page with new query after 2 seconds when stopped typing
+    $(function() {
+        var timer;
+        $(".searchInput").keyup(function() {
+            clearTimeout(timer);
+            timer = setTimeout(function() {
+                var val = $(".searchInput").val();
+                openPage("search.php?query=" + val);
+            }, 2000);
+        });
+    });
+</script>
