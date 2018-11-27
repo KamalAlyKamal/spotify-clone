5fb49f2f9b11e208ffb34356071560d541fd3935
Kamal Aly <kamalaly59@yahoo.com>
2018-11-28 00:15:36 +0200

    Create artist page top section layout

---
+++ artist.php
@@ -0,0 +1,23 @@
+<?php
+    include("Includes/includedFiles.php");
+
+    if(isset($_GET['id'])) {
+        $artistId = $_GET['id'];
+    }
+    else {
+        header("Location: index.php");
+    }
+
+    $artist = new Artist($con, $artistId);
+?>
+
+<div class="entityInfo">
+    <div class="centerSection">
+        <div class="artistInfo">
+            <h1 class="artistName"><?php echo $artist->getName(); ?></h1>
+            <div class="headerButtons">
+                <button class="button">PLAY</button>
+            </div>
+        </div>
+    </div>
+</div>
