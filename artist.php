fc16f7f844077775826b8fa36d011b7a3f67f769
Kamal Aly <kamalaly59@yahoo.com>
2018-11-28 00:22:47 +0200

    Style top section

--- artist.php
+++ artist.php
@@ -16,7 +16,7 @@
         <div class="artistInfo">
             <h1 class="artistName"><?php echo $artist->getName(); ?></h1>
             <div class="headerButtons">
-                <button class="button">PLAY</button>
+                <button class="button green">PLAY</button>
             </div>
         </div>
     </div>
