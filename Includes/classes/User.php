d2df60a72841cac760e9d6c8f5ec5cfff804ea18
Kamal Aly Kamal <kamalaly59@yahoo.com>
2018-11-29 19:05:19 +0200

    Display playlists

--- Includes/classes/User.php
+++ Includes/classes/User.php
@@ -7,7 +7,11 @@
 		public	function __construct($con, $username) {
 			$this->con = $con;
 			$this->username = $username;
-		}
+        }
+        
+        public function getUsername() {
+            return $this->username;
+        }
 
 	}
 
--- yourMusic.php
+++ yourMusic.php
@@ -8,5 +8,25 @@
         <div class="buttonItems">
             <button class="button green" onclick="createPlaylist('asdsadsad');">CREATE PLAYLIST</button>
         </div>
+
+        <?php 
+            $username = $userLoggedIn->getUsername();
+            $playlistsQuery = mysqli_query($con, "SELECT * FROM playlists WHERE owner='$username'");
+
+            if(mysqli_num_rows($playlistsQuery) == 0) {
+                echo "<p class='noResults'>You do not have any playlists.</p>";
+            }
+
+            while ($row = mysqli_fetch_array($playlistsQuery)) {
+                echo    "<div class='gridViewItem'>
+                            <div class='playlistImage'>
+                                <img src='assets/images/Icons/playlist.png'>
+                            </div>
+                            <div class='gridViewInfo'>"
+                                . $row['name'] .
+                            "</div>
+                        </div>";
+            }
+        ?>
     </div>
 </div>
